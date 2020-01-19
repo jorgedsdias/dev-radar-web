62c343f401dd1b3e70ab3d65d1eb8d21caa2b55a
Jorge Dias <jorgedsdias@gmail.com>
2020-01-19 12:26:28 -0300

    Updates

---
+++ precache-manifest.0f55df03b563437fba89e8eb657dcb52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e912d1f094ebc505c15cc9f721e96e",
+    "url": "/dev-radar-web/index.html"
+  },
+  {
+    "revision": "7156e165e2f71f9935fd",
+    "url": "/dev-radar-web/static/css/main.20ded2ff.chunk.css"
+  },
+  {
+    "revision": "366c5cf3ddad7f7de738",
+    "url": "/dev-radar-web/static/js/2.c4722873.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dev-radar-web/static/js/2.c4722873.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7156e165e2f71f9935fd",
+    "url": "/dev-radar-web/static/js/main.d859988f.chunk.js"
+  },
+  {
+    "revision": "fd01966f3c97ddbdffa6",
+    "url": "/dev-radar-web/static/js/runtime-main.0a378ea2.js"
+  }
+]);
