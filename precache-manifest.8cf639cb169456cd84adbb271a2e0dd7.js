353a8c62665fbc90c1de83173d94cb447dce9305
Jorge Dias <jorgedsdias@gmail.com>
2020-03-28 21:03:31 -0300

    Updates

---
+++ precache-manifest.8cf639cb169456cd84adbb271a2e0dd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c703dae40b741b6f86f16e30aa9357",
+    "url": "/dev-radar-web/index.html"
+  },
+  {
+    "revision": "bbd133f9eca0f22d8fd1",
+    "url": "/dev-radar-web/static/css/main.477fd8ca.chunk.css"
+  },
+  {
+    "revision": "46e412035fff63b5a77c",
+    "url": "/dev-radar-web/static/js/2.2511c1e5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dev-radar-web/static/js/2.2511c1e5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bbd133f9eca0f22d8fd1",
+    "url": "/dev-radar-web/static/js/main.359cdcda.chunk.js"
+  },
+  {
+    "revision": "fd01966f3c97ddbdffa6",
+    "url": "/dev-radar-web/static/js/runtime-main.0a378ea2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dev-radar-web/precache-manifest.93ea147f517a66abc90b39d2f114e14b.js"
+  "/dev-radar-web/precache-manifest.8cf639cb169456cd84adbb271a2e0dd7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dev-radar-web/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
